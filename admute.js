ff74db9f8cbec1c9343b3a19ac437eaea2a8295b
Adrastetion <86752500+Adrastetion@users.noreply.github.com>
2022-01-25 23:33:40 +0900

    create messages

--- admute.js
+++ admute.js
@@ -1,58 +1,54 @@
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function getElement(query) {
-    return new Promise(async (resolve) => {
-        let element = document.querySelector(query);
+  return new Promise(async (resolve) => {
+    let element = document.querySelector(query);
 
-        if(element) {
-            resolve(element);
-        } else {
-            await sleep(2000);
-            getElement(query).then(resolve);
-        }
-    });
+    if (element) {
+      resolve(element);
+    } else {
+      await sleep(2000);
+      getElement(query).then(resolve);
+    }
+  });
 }
 
 const config = { attributes: true };
 
-let muteState = 1; // 1 = unmuted, 0 = muted
-let adMute = 0; // to keep track if the player was muted by the extension or the user
+let isMuted = false;
+let adMute = false; // to keep track if the player was muted by the extension or the user
 let muteBtn;
 let nextBtn;
+let localizedNowplaying = blowser.i18n.getMessage(localizedNowplaying);
 
-
-Promise.all([
-    getElement('.player-controls__right'),
-    getElement('.volume-bar')
-])
-.then((elements) => {
+Promise.all([getElement(".player-controls__right"), getElement(".volume-bar")])
+  .then((elements) => {
     [playerControls, volumeBar] = elements;
 
     nextBtn = playerControls.children[0];
     muteBtn = volumeBar.children[0];
-    muteBtn.addEventListener('click', function() {
-        muteState = 1-muteState;
+    muteBtn.addEventListener("click", function () {
+      isMuted = !isMuted;
     });
 
-    return getElement('[aria-label*="Now playing"]');
-})
-.then((trackDiv) => {
-
-    let observer = new MutationObserver(function(mutations) {
-        if(nextBtn.hasAttribute('disabled')) {
-            if(muteState === 1) {
-                muteBtn.click();
-                adMute = 1;
-            }
-        } else if(adMute === 1 && muteState === 0) { // unmute only if muted by extension
-            muteBtn.click();
-            adMute = 0;
-        } else {
+    return getElement(`[aria-label*="${localizedNowplaying}"]`);
+  })
+  .then((trackDiv) => {
+    let observer = new MutationObserver(function (mutations) {
+      if (nextBtn.hasAttribute("disabled")) {
+        if (isMuted === false) {
+          muteBtn.click();
+          adMute = true;
         }
+      } else if (adMute === true && isMuted === true) {
+        // unmute only if muted by extension
+        muteBtn.click();
+        adMute = false;
+      } else {
+      }
     });
 
     observer.observe(trackDiv, config);
-});
-
+  });
